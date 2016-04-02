58aa9a1a2e3376243b99c5b5c27d3d90ce12de13
andela-mfalade <mayowa.falade@andela.com>
2016-04-02 23:56:29 +0100

    Set up initial project structure

---
+++ booker/booker_utils/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name="booker_utils",
+    description='Common utilities module',
+    packages=['booker_utils']
+)
---
+++ main.py
@@ -0,0 +1,8 @@
+# Connect to slack
+# Create connection to slack
+# Post message to slack
+# Read Message from slack
+
+# Connect to Google Calendar
+# Read available meeting times
+# Read all meeting rooms.
